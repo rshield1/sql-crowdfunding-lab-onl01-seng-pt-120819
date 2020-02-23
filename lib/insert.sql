03aa71b6256f5dda00ca8126e66c854cc7bc9953
Robert Shields <robert.shieldsiv@gmail.com>
2020-02-23 19:16:00 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,13 @@
+INSERT INTO users (name, age) VALUES ('Rob', 32), ('Ken', 30), ('Jared', 28), ('Erica', 35), ('Jade', 21), ('Raheem', 40), ('Mike', 33), ('Terra', 25), ('Kayla', 18), ('Albert', 45), ('Brian', 31), ('Nia', 28), ('Daryl', 32), ('Patrick', 52), ('Gloria', 50), ('Bianca', 17), ('Lilly', 58), ('Jeff', 47), ('Aaliyah', 33), ('Davida', 30);
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES 
+('Photography 101', 'photography', 50.00, '2020-04-14', '2020-07-28'),
+('Behind the Artist', 'music', 100.00, '2020-04-14', '2020-07-28'),
+('Ruby on Rails', 'technology', 20.00, '2020-04-14', '2020-07-28'),
+('Mindset', 'self-help', 40.00, '2020-04-14', '2020-07-28'),
+('TMZ News', 'entertainment', 10.00, '2020-04-14', '2020-07-28'),
+('Jay-Z & Beyonce', 'music', 110.00, '2020-04-14', '2020-07-28'),
+('React', 'technology', 120.00, '2020-04-14', '2020-07-28'),
+('Bootstrap CSS3', 'technology', 30.00, '2020-04-14', '2020-07-28'),
+('World of Warcraft Guide', 'games', 80.00, '2020-04-14', '2020-07-28'),
+('Final Fantasy', 'games', 120.00, '2020-04-14', '2020-07-28');
