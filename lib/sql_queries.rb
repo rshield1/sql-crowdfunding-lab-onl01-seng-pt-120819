6ec14eaaa9d8a7ccf8f78de217ed48c8b7c032a9
Robert Shields <robert.shieldsiv@gmail.com>
2020-02-23 19:55:18 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -15,7 +15,10 @@ def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-  "SELECT projects.title, (SUM(pledges.amount) - projects.funding_goal) AS amount_left FROM projects JOIN pledges ON projects.id = pledges.project_id GROUP BY projects.title HAVING amount_left >= 0;"
+  "SELECT projects.title, (projects.funding_goal - SUM(pledges.amount)) * -1 as amount_over_goal
+  FROM projects
+  INNER JOIN pledges ON projects.id = pledges.project_id GROUP BY projects.title
+  HAVING amount_over_goal >= 0"
 end
 end
 
